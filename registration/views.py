fc577cc0be0371caaadf4f382b0697b4864eb06b
bjoern nagel <bjoern.nagel@stud.fh-luebeck.de>
2018-06-23 13:56:20 +0200

    Erste eigne View ohne template

--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import render, render_to_response
 
 # Create your views here.
+from django.template.context import RequestContext
+
+def check_is_user_loggedin(request):
+    return "AnonymousUser" != request.user.__str__()
+
+
+def login(request):
+    if check_is_user_loggedin(request):
+        # _loggerinstance.log("Logged in user accessed Index-Page - (%s)" % request.user.username, "INFO")
+        # return HttpResponseRedirect("/backpackpage")
+        return HttpResponseRedirect("/")
+    else:
+        requestcontext = RequestContext(request)
+        return render_to_response("index.html", requestcontext)
+    #     _loggerinstance.log("Anonymous user accessed Index-Page", "INFO")
+    #
+    # c = _configuration.get_TrackingDictionary()
+    # c.update(csrf(request))
+    # return render_to_response("index.html", c)
+
+"""
+def view_registration_page(request):
+    if check_is_user_loggedin(request):
+        _loggerinstance.log("Logged in user accessed Registration-Page - (%s)" % request.user.username, "INFO")
+        return HttpResponseRedirect("/backpackpage")
+    else:
+        _loggerinstance.log("Anonymous user accessed Registration-Page", "INFO")
+
+        c = _configuration.get_TrackingDictionary()
+        c.update(csrf(request))
+        c["CurrentPopulation"] = shipAPI.get_current_population()
+        c["MaximumPopulation"] = shipAPI.get_maximum_population()
+        return render_to_response("registration.html", c)
+
+@LoginValidator
+def login_user(request):
+"""
