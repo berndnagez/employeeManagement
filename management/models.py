4dcd4763c009ed525cf2d1d3f5d5c2897fc31ec5
bjoern nagel <bjoern.nagel@stud.fh-luebeck.de>
2018-06-24 08:20:08 +0200

    Tables angelegt und Model verbessert

--- management/models.py
+++ management/models.py
@@ -4,11 +4,15 @@ from django.db import models
 
 
 class Employee(models.Model):
-    name = models.CharField(max_length=30)
     firstName = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
     birthDay = models.DateField()
     street = models.CharField(max_length=30)
     houseNum = models.IntegerField()
+    city = models.CharField(max_length=30, default='Hamburg')
     postCode = models.IntegerField()
-    overtime = models.IntegerField()
-    leaveDays = models.IntegerField()
+    overtime = models.IntegerField(default='0')
+    leaveDays = models.IntegerField(default='30')
+
+    def __str__(self):
+        return self.name + ', ' + self.firstName + ' (ID: ' + str((self.id)) + ')'
