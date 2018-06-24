de794b93090bb6bd0bddba7862deaea286eb5fbe
bjoern nagel <bjoern.nagel@stud.fh-luebeck.de>
2018-06-24 07:27:16 +0200

    Mitarbeiter_Innen-Model erstellt

--- registration/admin.py
+++ registration/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from registration.models import Employee
 
 # Register your models here.
+
+admin.register(Employee)
--- registration/models.py
+++ registration/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=30)
+    firstName = models.CharField(max_length=30)
+    birthDay = models.DateField()
+    street = models.CharField(max_length=30)
+    houseNum = models.IntegerField()
+    postCode = models.IntegerField()
+    overtime = models.IntegerField()
+    leaveDays = models.IntegerField()
